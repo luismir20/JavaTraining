4e4f2301c1659b585596597bcb5074a57043f1ad
Luis Mireles <data.luis13@gmail.com>
2021-06-30 11:02:12 -0500

    updates

---
+++ src/main/java/day1/ex3/PrimitiveResults.java
@@ -0,0 +1,17 @@
+package day1.ex3;
+
+public class PrimitiveResults {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Primitives p1 = null;
+		p1 = new Primitives();
+		p1.name = "Luis";
+		p1.age = 25;
+		p1.weight = 160;
+		p1.printPrimitive();
+
+	}
+
+}
---
+++ src/main/java/day1/ex3/Primitives.java
@@ -0,0 +1,12 @@
+package day1.ex3;
+
+public class Primitives {
+
+	int age;
+	String name;
+	double weight;
+
+	public void printPrimitive() {
+		System.out.println("name: " + name + " age: " + age + " weight: " + weight);
+	}
+}
