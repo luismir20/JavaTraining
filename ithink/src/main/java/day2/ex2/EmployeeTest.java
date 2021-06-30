a44772d008f6c001e7181f4692faf9b94a551079
Luis Mireles <data.luis13@gmail.com>
2021-06-30 17:15:25 -0500

    adding stuff

--- ithink/src/main/java/day2/ex2/EmployeeTest.java
+++ ithink/src/main/java/day2/ex2/EmployeeTest.java
@@ -5,26 +5,24 @@ public class EmployeeTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Employee emp1 = new Employee("Luis", "Thinker", 5000.99);
-        Employee emp2 = new Employee("You", "Dreamer", 5000.99);
+		Employee emp2 = new Employee("You", "Dreamer", 5000.99);
 
-        System.out.println("1st Employee's first name: " + emp1.getFirstName());
-        System.out.println("1st Employee's last name: " + emp1.getLastName());
-        System.out.println("1st Employee's monthly salary: " + emp1.getMonthlySalary());
-        System.out.println();
+		System.out.println("1st Employee's first name: " + emp1.getFirstName());
+		System.out.println("1st Employee's last name: " + emp1.getLastName());
+		System.out.println("1st Employee's monthly salary: " + emp1.getMonthlySalary());
+		System.out.println();
 
-        System.out.println("2nd Employee's first name: " + emp2.getFirstName());
-        System.out.println("2nd Employee's last name: " + emp2.getLastName());
-        System.out.println("2nd Employee's monthly salary: " + emp2.getMonthlySalary());
-        System.out.println();
+		System.out.println("2nd Employee's first name: " + emp2.getFirstName());
+		System.out.println("2nd Employee's last name: " + emp2.getLastName());
+		System.out.println("2nd Employee's monthly salary: " + emp2.getMonthlySalary());
+		System.out.println();
 
-        emp1.setMonthlySalary(1.1 * emp1.getMonthlySalary());
-        emp2.setMonthlySalary(1.1 * emp2.getMonthlySalary());
+		emp1.setMonthlySalary(1.1 * emp1.getMonthlySalary());
+		emp2.setMonthlySalary(1.1 * emp2.getMonthlySalary());
 
-        System.out.println("1st Employee's monthly salary after a 10% raise:" + emp2.getMonthlySalary());
-        System.out.println("2nd Employee's monthly salary after a 10% raise:" + emp2.getMonthlySalary());
+		System.out.println("1st Employee's monthly salary after a 10% raise:" + emp2.getMonthlySalary());
+		System.out.println("2nd Employee's monthly salary after a 10% raise:" + emp2.getMonthlySalary());
 
-    }
+	}
 
 }
-
-
---
+++ ithink/src/main/java/percipiopractice/ShoppingCart.java
@@ -0,0 +1,10 @@
+package percipiopractice;
+
+public class ShoppingCart {
+
+	public static void main(String[] args) {
+		//instantiate a Shirt object and call the display() method
+		Shirt shirt = new Shirt (25.99, 'M', 'R');
+		shirt.display();
+	}
+}
