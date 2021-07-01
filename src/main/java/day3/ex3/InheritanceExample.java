412c5f14d2255374a368367e500ee928faf7dc0d
Luis Mireles <data.luis13@gmail.com>
2021-07-01 13:02:22 -0500

    updates to day ex 3

--- src/main/java/day3/ex3/InheritanceExample.java
+++ src/main/java/day3/ex3/InheritanceExample.java
@@ -1,33 +1,31 @@
 package day3.ex3;
 
-	class A {
-	    void method_test() {
-	        System.out.println("I am method from class A");
-	    }
+class A {
+	void method_test() {
+		System.out.println("Class A method");
+	}
+}
+
+class B extends A {
+
+	public B() {
+		super.method_test();
+		method_test();
+		System.out.println();
 	}
 
-	class B extends A {
-
-	    public B() {
-	        super.method_test();
-	        method_test();
-	        System.out.println();
-	    }
-
-	    void method_test() {
-	        System.out.println("I am method from class B");
-	    }
-
+	void method_test() {
+		System.out.println("Class B method");
 	}
 
-	public class InheritanceExample {
+}
 
-	    public static void main(String[] args) {
-	        B test = new B();
+public class InheritanceExample {
 
-	        test.method_test();
-	    }
+	public static void main(String[] args) {
+		B test = new B();
 
+		test.method_test();
 	}
-	
 
+}
