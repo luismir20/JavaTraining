5294c165f1adfdae7d3802b11f97e8be30279255
Luis Mireles <data.luis13@gmail.com>
2021-07-07 09:53:45 -0500

    updates

--- src/main/java/HackerRank/Ex1.java
+++ src/main/java/HackerRank/Ex1.java
@@ -3,7 +3,7 @@ package HackerRank;
 public class Ex1 {
 
     public static void main(String[] args) {
-    System.out.println("Hello, World.");
+    System.out.println("Hello, World!");
     System.out.println("Hello, Java.");
     }
 }
